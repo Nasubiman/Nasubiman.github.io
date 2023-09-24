985b7e3565ea6326a52bfc582c3ec44d9d86c515
nasubiman <nasubimanza@gmail.com>
2023-09-24 23:20:20 +0900

    よく分からん

--- index.js
+++ index.js
@@ -1,59 +1,56 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const num1Input = document.getElementById("num1") as HTMLInputElement;
-//     const num2Input = document.getElementById("num2") as HTMLInputElement;
-//     const calculateButton = document.getElementById("calculateButton");
-//     const resultParagraph = document.getElementById("result");
-//     calculateButton?.addEventListener("click", () => {
-//         const num1 = parseFloat(num1Input.value);
-//         const num2 = parseFloat(num2Input.value);
-//         if (!isNaN(num1) && !isNaN(num2)) {
-//             const sum = num1 + num2;
-//             resultParagraph.textContent = `結果: ${sum}`;
-//         } else {
-//             resultParagraph.textContent = "有効な数値を入力してください";
-//         }
-//     });
-// });
-// HTML要素への参照を取得
-var num1Input = document.getElementById("num1");
-var num2Input = document.getElementById("num2");
-var calculateButton = document.getElementById("calculateButton");
-var resultParagraph = document.getElementById("result");
-// ボタンクリック時の処理
-calculateButton.addEventListener("click", function () {
-    // 入力値を取得し数値に変換
-    var num1 = parseFloat(num1Input.value);
-    var num2 = parseFloat(num2Input.value);
-    // 数値が有効な場合、足し算を行って結果を表示
-    if (!isNaN(num1) && !isNaN(num2)) {
-        var result = num1 + num2;
-        resultParagraph.textContent = "\u7D50\u679C: ".concat(result);
-    }
-    else {
-        resultParagraph.textContent = "無効な入力です。数値を入力してください。";
-    }
-});
-var Calculator = /** @class */ (function () {
-    function Calculator() {
-    }
-    // 足し算
-    Calculator.add = function (x, y) {
-        return x + y;
-    };
-    // 引き算
-    Calculator.subtract = function (x, y) {
-        return x - y;
-    };
-    // 掛け算
-    Calculator.multiply = function (x, y) {
-        return x * y;
-    };
-    // 割り算
-    Calculator.divide = function (x, y) {
-        if (y === 0) {
-            throw new Error("0で割ることはできません。");
+document.addEventListener("DOMContentLoaded", function () {
+    var num1Input = document.getElementById("num1");
+    var num2Input = document.getElementById("num2");
+    var calculateButton = document.getElementById("calculateButton");
+    var resultParagraph = document.getElementById("result");
+    calculateButton === null || calculateButton === void 0 ? void 0 : calculateButton.addEventListener("click", function () {
+        var num1 = parseFloat(num1Input.value);
+        var num2 = parseFloat(num2Input.value);
+        if (!isNaN(num1) && !isNaN(num2)) {
+            var sum = num1 + num2;
+            resultParagraph.textContent = "\u7D50\u679C: ".concat(sum);
         }
-        return x / y;
-    };
-    return Calculator;
-}());
+        else {
+            resultParagraph.textContent = "有効な数値を入力してください";
+        }
+    });
+});
+// // HTML要素への参照を取得
+// const num1Input = document.getElementById("num1") as HTMLInputElement;
+// const num2Input = document.getElementById("num2") as HTMLInputElement;
+// const calculateButton = document.getElementById("calculateButton") as HTMLButtonElement;
+// const resultParagraph = document.getElementById("result") as HTMLParagraphElement;
+// // ボタンクリック時の処理
+// calculateButton.addEventListener("click", () => {
+//     // 入力値を取得し数値に変換
+//     const num1 = parseFloat(num1Input.value);
+//     const num2 = parseFloat(num2Input.value);
+//     // 数値が有効な場合、足し算を行って結果を表示
+//     if (!isNaN(num1) && !isNaN(num2)) {
+//         const result = num1 + num2;
+//         resultParagraph.textContent = `結果: ${result}`;
+//     } else {
+//         resultParagraph.textContent = "無効な入力です。数値を入力してください。";
+//     }
+// });
+// class Calculator {
+//     // 足し算
+//     static add(x: number, y: number): number {
+//         return x + y;
+//     }
+//     // 引き算
+//     static subtract(x: number, y: number): number {
+//         return x - y;
+//     }
+//     // 掛け算
+//     static multiply(x: number, y: number): number {
+//         return x * y;
+//     }
+//     // 割り算
+//     static divide(x: number, y: number): number {
+//         if (y === 0) {
+//             throw new Error("0で割ることはできません。");
+//         }
+//         return x / y;
+//     }
+// }
--- index.ts
+++ index.ts
@@ -1,67 +1,67 @@
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     const num1Input = document.getElementById("num1") as HTMLInputElement;
-//     const num2Input = document.getElementById("num2") as HTMLInputElement;
-//     const calculateButton = document.getElementById("calculateButton");
-//     const resultParagraph = document.getElementById("result");
+document.addEventListener("DOMContentLoaded", () => {
+    const num1Input = document.getElementById("num1") as HTMLInputElement;
+    const num2Input = document.getElementById("num2") as HTMLInputElement;
+    const calculateButton = document.getElementById("calculateButton");
+    const resultParagraph = document.getElementById("result");
 
-//     calculateButton?.addEventListener("click", () => {
-//         const num1 = parseFloat(num1Input.value);
-//         const num2 = parseFloat(num2Input.value);
+    calculateButton?.addEventListener("click", () => {
+        const num1 = parseFloat(num1Input.value);
+        const num2 = parseFloat(num2Input.value);
 
-//         if (!isNaN(num1) && !isNaN(num2)) {
-//             const sum = num1 + num2;
-//             resultParagraph.textContent = `結果: ${sum}`;
-//         } else {
-//             resultParagraph.textContent = "有効な数値を入力してください";
-//         }
-//     });
-// });
-
-
-// HTML要素への参照を取得
-const num1Input = document.getElementById("num1") as HTMLInputElement;
-const num2Input = document.getElementById("num2") as HTMLInputElement;
-const calculateButton = document.getElementById("calculateButton") as HTMLButtonElement;
-const resultParagraph = document.getElementById("result") as HTMLParagraphElement;
-
-// ボタンクリック時の処理
-calculateButton.addEventListener("click", () => {
-    // 入力値を取得し数値に変換
-    const num1 = parseFloat(num1Input.value);
-    const num2 = parseFloat(num2Input.value);
-
-    // 数値が有効な場合、足し算を行って結果を表示
-    if (!isNaN(num1) && !isNaN(num2)) {
-        const result = num1 + num2;
-        resultParagraph.textContent = `結果: ${result}`;
-    } else {
-        resultParagraph.textContent = "無効な入力です。数値を入力してください。";
-    }
+        if (!isNaN(num1) && !isNaN(num2)) {
+            const sum = num1 + num2;
+            resultParagraph.textContent = `結果あああ: ${sum}`;
+        } else {
+            resultParagraph.textContent = "有効な数値を入力してください";
+        }
+    });
 });
 
 
-class Calculator {
-    // 足し算
-    static add(x: number, y: number): number {
-        return x + y;
-    }
+// // HTML要素への参照を取得
+// const num1Input = document.getElementById("num1") as HTMLInputElement;
+// const num2Input = document.getElementById("num2") as HTMLInputElement;
+// const calculateButton = document.getElementById("calculateButton") as HTMLButtonElement;
+// const resultParagraph = document.getElementById("result") as HTMLParagraphElement;
 
-    // 引き算
-    static subtract(x: number, y: number): number {
-        return x - y;
-    }
+// // ボタンクリック時の処理
+// calculateButton.addEventListener("click", () => {
+//     // 入力値を取得し数値に変換
+//     const num1 = parseFloat(num1Input.value);
+//     const num2 = parseFloat(num2Input.value);
 
-    // 掛け算
-    static multiply(x: number, y: number): number {
-        return x * y;
-    }
+//     // 数値が有効な場合、足し算を行って結果を表示
+//     if (!isNaN(num1) && !isNaN(num2)) {
+//         const result = num1 + num2;
+//         resultParagraph.textContent = `結果: ${result}`;
+//     } else {
+//         resultParagraph.textContent = "無効な入力です。数値を入力してください。";
+//     }
+// });
 
-    // 割り算
-    static divide(x: number, y: number): number {
-        if (y === 0) {
-            throw new Error("0で割ることはできません。");
-        }
-        return x / y;
-    }
-}
+
+// class Calculator {
+//     // 足し算
+//     static add(x: number, y: number): number {
+//         return x + y;
+//     }
+
+//     // 引き算
+//     static subtract(x: number, y: number): number {
+//         return x - y;
+//     }
+
+//     // 掛け算
+//     static multiply(x: number, y: number): number {
+//         return x * y;
+//     }
+
+//     // 割り算
+//     static divide(x: number, y: number): number {
+//         if (y === 0) {
+//             throw new Error("0で割ることはできません。");
+//         }
+//         return x / y;
+//     }
+// }
