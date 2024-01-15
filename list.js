7b7ac49293cd7dcdffcad5318fc4b90294157b66
nasubiman <nasubimanza@gmail.com>
2024-01-15 13:20:39 +0900

    いろいろやった

--- list.js
+++ list.js
@@ -1,23 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
     var myButton = document.getElementById("myButton");
-    var listInput = document.getElementById('myInput');
+    var listInput = document.getElementById('waterway');
     var playerName = document.getElementById('playerName');
     var tuples = [];
     function listControl(operation) {
         switch (operation) {
-            case "addList":
+            case "addElement":
                 if (!isNaN(parseInt(listInput.value))) {
                     var tuple = [playerName.value, parseInt(listInput.value)];
                     tuples.push(tuple);
                     var li = document.createElement('li');
-                    // li.textContent = (document.getElementById('myInput') as HTMLInputElement).value;
-                    li.textContent = tuple[0][0] + "   水路 " + tuple[1][0];
+                    // li.textContent = (document.getElementById('waterway') as HTMLInputElement).value;
+                    li.textContent = tuple[0] + "   水路 " + tuple[1];
                     document.getElementById('myList').appendChild(li);
                     document.getElementById('playerName').value = '';
-                    document.getElementById('myInput').value = '';
+                    document.getElementById('waterway').value = '';
                 }
+                break;
+            case "removeElement":
                 break;
         }
     }
-    myButton === null || myButton === void 0 ? void 0 : myButton.addEventListener("click", function () { return listControl("addList"); });
+    myButton === null || myButton === void 0 ? void 0 : myButton.addEventListener("click", function () { return listControl("addElement"); });
 });
--- list.ts
+++ list.ts
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const myButton = document.getElementById("myButton");
-    const listInput = document.getElementById('myInput') as HTMLInputElement;
+    const listInput = document.getElementById('waterway') as HTMLInputElement;
     const playerName = document.getElementById('playerName') as HTMLInputElement;
 
     let tuples: [string, number][] = [];
@@ -9,25 +9,28 @@ document.addEventListener("DOMContentLoaded", () => {
     function listControl(operation){
 
         switch(operation){
-            case "addList":
+            case "addElement":
                 if (!isNaN(parseInt(listInput.value))) {
+                    
                     let tuple: [string, number] = [playerName.value , parseInt(listInput.value)];
                     tuples.push(tuple);
 
                     const li = document.createElement('li');
 
-                    // li.textContent = (document.getElementById('myInput') as HTMLInputElement).value;
+                    // li.textContent = (document.getElementById('waterway') as HTMLInputElement).value;
                     li.textContent = tuple[0] + "   水路 " +  tuple[1];
                     document.getElementById('myList')!.appendChild(li);
                     (document.getElementById('playerName') as HTMLInputElement).value = '';
-                    (document.getElementById('myInput') as HTMLInputElement).value = '';
+                    (document.getElementById('waterway') as HTMLInputElement).value = '';
                 }
+                break;
+            case "removeElement":
                 break;
 
         }
 
     }
 
-    myButton?.addEventListener("click", () => listControl("addList"));
+    myButton?.addEventListener("click", () => listControl("addElement"));
 
 });
